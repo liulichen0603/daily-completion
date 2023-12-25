fd58b8f338ff9b28d8499c60856820ae8fd71c51
Lichen <liulichen0603@163.com>
2023-12-25 15:49:53 +0800

    update ui for chart tab, not completed

--- lib/ui/chart_tab.dart
+++ lib/ui/chart_tab.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class ChartTab extends StatefulWidget {
-  const ChartTab({super.key});
-
-  @override
-  State<ChartTab> createState() => _ChartTabState();
-}
-
-class _ChartTabState extends State<ChartTab> {
+class ChartTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Container(
+        height: 300,
+        margin: EdgeInsets.all(16),
+        child: BarChart(
+          BarChartData(
+            barGroups: _createSampleData(),
+            titlesData: FlTitlesData(
+              leftTitles: SideTitles(showTitles: true),
+              bottomTitles: SideTitles(showTitles: true),
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  List<BarChartGroupData> _createSampleData() {
+    return [
+      BarChartGroupData(
+        x: 0,
+        barRods: [
+          BarChartRodData(
+            y: 5,
+            color: Colors.blue,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 1,
+        barRods: [
+          BarChartRodData(
+            y: 25,
+            color: Colors.green,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 2,
+        barRods: [
+          BarChartRodData(
+            y: 100,
+            color: Colors.orange,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 3,
+        barRods: [
+          BarChartRodData(
+            y: 75,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    ];
+  }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fl Chart Example'),
+        ),
+        body: ChartTab(),
+      ),
+    ),
+  );
 }
